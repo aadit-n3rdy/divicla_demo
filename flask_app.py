d54eea447d0d1e6915551f77b8470a9e3026e362
Aadithyaa <eaadithyaa@gmail.com>
2025-04-01 11:28:22 +0530

    Websocket client

--- flask_app.py
+++ flask_app.py
@@ -4,16 +4,19 @@ import psutil
 import threading
 import asyncio
 from websockets.sync.server import serve
+from websockets.sync.client import connect
 import time
 import json
 
 app = Flask(__name__)
 socketio = SocketIO(app)
 
+
 # Serve the webpage
-@app.route('/')
+@app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
+
 
 class Data:
     def __init__(self, conn):
@@ -22,30 +25,31 @@ class Data:
         self.alive = False
         self.bench = False
         self.conn = conn
+
     def add(self, util, alive, bench):
         self.time = time.time()
         self.util = util
         self.alive = alive
         self.bench = bench
 
+
 stateDict = {}
+
 
 # WebSocket server to emit CPU utilization data
 def emit_cpu_utilization():
     while True:
         state = {}
         for k, v in stateDict.items():
-            state[k] = {
-                "util": v.util,
-                "alive": v.alive,
-                "bench": v.bench
-            }
-        socketio.emit('cpu_update', json.dumps(state))
+            state[k] = {"util": v.util, "alive": v.alive, "bench": v.bench}
+        socketio.emit("cpu_update", json.dumps(state))
         time.sleep(1)
+
 
 emitThread = threading.Thread(target=emit_cpu_utilization)
 emitThread.daemon = True
 emitThread.start()
+
 
 @socketio.on("control")
 def handleToggle(msg):
@@ -55,6 +59,7 @@ def handleToggle(msg):
     print(f"Received command {cmd} for {nodeId}")
     stateDict[nodeId].conn.send(cmd)
     print(f"Sent {cmd} to {nodeId}")
+
 
 def handler(websocket):
     print("New conn")
@@ -69,14 +74,33 @@ def handler(websocket):
         bench = bool(int(bench))
         stateDict[nodeId].add(util, alive, bench)
 
+
 # Start the WebSocket server
 def start_websocket_server():
-    srv = serve(handler, "", 6000)
-    print("Starting WebSocket server")
-    srv.serve_forever()
-    print("This shouldn't print")
+    # srv = serve(handler, "", 6000)
+    # print("Starting WebSocket server")
+    # srv.serve_forever()
+    # print("This shouldn't print")
+    pass
+
+
+node1IP = "ws://172.17.149.27:6000"
+node2IP = "ws://172.17.149.28:6000"
 
 # Start both Flask and WebSocket servers in separate threads
-if __name__ == '__main__':
-    threading.Thread(target=start_websocket_server, daemon=True).start()
-    socketio.run(app, host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    # Connect to the nodes
+    node1Conn = connect(node1IP)
+    node2Conn = connect(node2IP)
+    # Create Data objects for each node
+    stateDict["node1"] = Data(node1Conn)
+    stateDict["node2"] = Data(node2Conn)
+
+    t1 = threading.Thread(target=handler, args=(node1Conn,))
+    t2 = threading.Thread(target=handler, args=(node2Conn,))
+    t1.daemon = True
+    t2.daemon = True
+    t1.start()
+    t2.start()
+
+    socketio.run(app, host="0.0.0.0", port=5000)
